1ffae4709f16533ccd6c225ce23a830cba228e3e
MatthiasCruciani <matthiascruciani@hotmail.fr>
2021-06-24 11:48:05 +0200

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,7 +10,6 @@ export class AuthService {
 
   twitchAuth(code: string) {
     const url = `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${secret}&code=${code}&grant_type=authorization_code&redirect_uri=http://localhost:4200`;
-    console.log('GUCCI_twitchAuth', code, url);
     return this.http.post<TwitchAuth>(url).toPromise()
       .then(x => x.data)
       .then(y => this.http.get('https://api.twitch.tv/helix/users', {headers: {'client-id': clientId, 'authorization': 'Bearer ' + y.access_token}}).toPromise().then(r => {
